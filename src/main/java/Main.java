f16182c3dbd8ff679ad6c8881692c13457a1708b
etern <Invincible!3>
2019-11-03 15:28:51 -0600

    Added the first few screens of the GUI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-
+        new LoginScreen();
 
 Loan a1 = new Loan(5000, 3579,.15,  95.83, LocalDate.of(2019,10,27),
         LocalDate.of(2019,10,1),LocalDate.of(2019,8,20));
