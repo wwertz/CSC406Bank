001eaa0772c728d6bbe745e67981993357323922
Celery1969 <32178133+Celery1969@users.noreply.github.com>
2019-11-12 19:57:59 -0600

    moved saving methods to checking and saving and added reading methods

--- src/main/java/Saving.java
+++ src/main/java/Saving.java
@@ -1,3 +1,9 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.time.*;
 
 //********************************still needs interest****************************************8
@@ -52,4 +58,44 @@ public class Saving extends  Account{
     public double getBalance() {return balance;}
     public void setBalance(int balance) {this.balance = balance;}
 
+    public void JSONSavToTxt(int AID, int CID, double balance, char type, LocalDate CDdate, double interestRate){
+
+        JSONObject obj = new JSONObject();
+
+        obj.put("AccountID", AID);
+        obj.put("CustomerID", CID);
+        obj.put("Balance", balance);
+        obj.put("type", type);
+        obj.put("CDdate", CDdate);
+        obj.put("interestRate", interestRate);
+
+        try (PrintWriter file = new PrintWriter("/Savings/"+AID +".txt")) {
+            file.write(obj.toJSONString());
+            System.out.println("Successfully wrote savings "+AID+" to file");
+            System.out.println("\nJSON Object: " + obj);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unsuccessful savings Account Write for "+AID);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void JSONTxtToSav(int AID) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader(
+                    "/Savings/"+AID +".txt"));
+
+            JSONObject SAVjsonObject = (JSONObject) obj;
+
+            int AccountID = (int) SAVjsonObject.get("AccountID");
+            int CustomerID = (int) SAVjsonObject.get("CustomerID");
+            double Balance = (double) SAVjsonObject.get("Balance");
+            char type = (char) SAVjsonObject.get("type");
+            LocalDate CDdate = (LocalDate) SAVjsonObject.get("CDdate");
+            double interestRate = (double) SAVjsonObject.get("interestRate");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }//end of Saving
